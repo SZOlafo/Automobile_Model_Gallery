3218707caa335e56e6e4ffdcf80d88df63aea2f8
Olaf <olaf.szajda@gmail.com>
2025-04-02 00:18:54 +0200

    #26 added 3D rendering to deail view

---
+++ src/AutomobileGallery.frontend/src/components/CarModel.jsx
@@ -0,0 +1,36 @@
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { useEffect } from "react";
+import SceneInit from "../lib/SceneInit.js"
+
+export function CarModel({ modelPath }) {
+  useEffect(() => {
+    const scene = new SceneInit('dupa');
+    scene.initialize();
+    scene.animate();
+
+    let Model = new THREE.Object3D();
+    const glftLoader = new GLTFLoader();
+    glftLoader.load(modelPath, (gltf) => {
+      Model.add(gltf.scene)
+      Model.scale.set(1500, 1500, 1500);
+
+      scene.scene.add(Model);
+    });
+
+    const animate = () => {
+      if (Model) {
+        Model.rotation.y += 0.01;
+      }
+      requestAnimationFrame(animate);
+    };
+    animate();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <div >
+      <canvas style={{width:"80vw"}} id="dupa" />
+    </div>
+  );
+}
--- src/AutomobileGallery.frontend/src/pages/DetailsView.jsx
+++ src/AutomobileGallery.frontend/src/pages/DetailsView.jsx
@@ -1,6 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import "../css/DetailsView.css"
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import { CarModel } from "../components/CarModel";
 
 export default function DetailsView() {
 
@@ -14,11 +15,12 @@ export default function DetailsView() {
         <ArrowBackIcon id="arrow" onClick={backToShop}></ArrowBackIcon>
         <div id='product'>
             <figure id="image-product" className='bg-image'>
-                <img
+                {/* <img
                     src='https://img.freepik.com/free-psd/red-isolated-car_23-2151852884.jpg'
                     className='img-fluid rounded shadow-3'
                     alt='...'
-                />
+                /> */}
+                <CarModel modelPath={'assets/golf.glb'} />
                 <h1 id="caption">Test</h1>
             </figure>
         </div>
